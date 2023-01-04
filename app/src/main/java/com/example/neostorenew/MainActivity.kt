f52e990f140c83fadaf89ccd7a01cc598b92102f
saurabhkul11 <saurabhkul18@gmail.com>
2023-01-04 11:29:29 +0530

    Register activity file is committed

--- app/src/main/java/com/example/neostorenew/MainActivity.kt
+++ app/src/main/java/com/example/neostorenew/MainActivity.kt
@@ -2,21 +2,10 @@ package com.example.neostorenew
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import com.example.neostorenew.base.BaseActivity
-import com.example.neostorenew.databinding.ActivityMainBinding
 
-class MainActivity : BaseActivity<ActivityMainBinding>() {
-
-
-    override fun showToast(message: String) {
-        super.showToast("MainActivity")
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
-
-
-    override fun inflateLayout(layoutInflater: LayoutInflater)= ActivityMainBinding.inflate(layoutInflater)
-    override fun applyUI(binding: ActivityMainBinding) {
-    }
-
 }
-
--- app/src/main/java/com/example/neostorenew/utils/Constant/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.example.neostorenew.utils.Constant
-
-object Constants {
-
-    val BASE_URL="http://staging.php-dev.in:8844/trainingapp/api/"
-    val ACCESS_TOKEN="access_token"
-
-}
